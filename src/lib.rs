bdccf36bd0e55d973126af8c148dc1dcdcb02d44
Tomas Krnak <tomas@krnak.cz>
2022-10-07 10:09:57 +0200

    add TrezorBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,235 @@
+use nonempty::NonEmpty;
+use orchard::{
+    self,
+    builder::{InProgress, SigningMetadata, SigningParts, SpendInfo, Unauthorized, Unproven},
+    bundle::{Bundle, Flags},
+    circuit::Circuit,
+    keys::{FullViewingKey, Scope, SpendValidatingKey},
+    note::{Note, TransmittedNoteCiphertext},
+    note_encryption::{OrchardDomain, OrchardNoteEncryption},
+    tree::{Anchor, MerklePath},
+    value::{NoteValue, ValueCommitTrapdoor, ValueCommitment, ValueSum},
+    Action, Address,
+};
+use pasta_curves::group::ff::PrimeField;
+use rand::thread_rng;
+use zcash_address::{
+    unified::{Address as UnifiedAddress, Container, Encoding, Receiver},
+    Network,
+};
+use zcash_note_encryption::Domain;
+use zcash_primitives::memo::Memo;
+
+mod random;
+use random::{ActionShieldingRng, BundleShieldingRng, MockEncryptionRng};
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod tests;
+
+#[derive(Debug, Clone)]
+pub struct OrchardInput {
+    pub note: Note,
+    pub merkle_path: MerklePath,
+}
+
+impl OrchardInput {
+    pub fn dummy(rng: &ActionShieldingRng) -> Self {
+        let sk = rng.dummy_sk();
+        let fvk: FullViewingKey = (&sk).into();
+        let recipient = fvk.address_at(0u64, Scope::External);
+        let rho = rng.dummy_rho();
+        let note = Note::from_parts(
+            recipient,
+            NoteValue::from_raw(0u64),
+            rho,
+            rng.dummy_rseed_old(&rho),
+        );
+        let merkle_path = MerklePath::dummy(&mut thread_rng());
+        OrchardInput { note, merkle_path }
     }
+}
+
+#[derive(Clone, Debug)]
+pub enum Recipient {
+    External(String),
+    Change,
+    Dummy,
+}
+
+#[derive(Clone, Debug)]
+pub struct OrchardOutput {
+    pub recipient: Recipient,
+    pub amount: u64,
+    pub memo: Option<String>,
+}
+
+impl OrchardOutput {
+    fn dummy() -> Self {
+        OrchardOutput {
+            recipient: Recipient::Dummy,
+            amount: 0,
+            memo: None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TrezorBuilder {
+    pub inputs: Vec<Option<OrchardInput>>,
+    pub outputs: Vec<Option<OrchardOutput>>,
+    pub anchor: Anchor,
+    pub fvk: FullViewingKey,
+    pub shielding_seed: [u8; 32],
+}
+
+impl TrezorBuilder {
+    pub fn build<V: TryFrom<i64>>(
+        mut self,
+    ) -> Result<Bundle<InProgress<Unproven, Unauthorized>, V>, String> {
+        let bundle_rng = BundleShieldingRng(self.shielding_seed);
+        assert!(
+            self.inputs.len() + self.outputs.len() > 0,
+            "bundle is empty"
+        );
+        let actions_count: usize = [self.inputs.len(), self.outputs.len(), 2]
+            .into_iter()
+            .max()
+            .unwrap();
+        pad(&mut self.inputs, actions_count);
+        pad(&mut self.outputs, actions_count);
+        bundle_rng.shuffle_inputs(&mut self.inputs);
+        bundle_rng.shuffle_outputs(&mut self.outputs);
+        let mut circuits: Vec<Circuit> = vec![];
+        let mut actions: Vec<Action<_>> = vec![];
+        let mut balance = ValueSum::zero();
+        let mut rcv_sum = ValueCommitTrapdoor::from_bytes([0u8; 32]).unwrap();
+        for (i, (maybe_input, maybe_output)) in self
+            .inputs
+            .into_iter()
+            .zip(self.outputs.into_iter())
+            .enumerate()
+        {
+            let rng = bundle_rng.for_action(i as u32);
+            let (input, fvk, dummy_ask) = match maybe_input {
+                Some(input) => (input, self.fvk.clone(), None),
+                None => (
+                    OrchardInput::dummy(&rng),
+                    (&rng.dummy_sk()).into(),
+                    Some(rng.dummy_ask()),
+                ),
+            };
+
+            let output = maybe_output.unwrap_or_else(|| OrchardOutput::dummy());
+
+            let (recipient, scope, ovk) = match output.recipient {
+                Recipient::External(addr) => (
+                    parse_u_address(addr)?,
+                    Scope::External,
+                    Some(self.fvk.to_ovk(Scope::External)),
+                ),
+                Recipient::Change => (
+                    (&self.fvk).address_at(0u64, Scope::Internal),
+                    Scope::Internal,
+                    Some(self.fvk.to_ovk(Scope::Internal)),
+                ),
+                Recipient::Dummy => (rng.dummy_recipient(), Scope::External, None),
+            };
+
+            let memo: Memo = match output.memo {
+                None => Memo::Empty,
+                Some(x) => x.parse().map_err(|_| "cannot encode memo".to_owned())?,
+            };
+            let memo = memo.encode().as_array().clone();
+
+            let rcv = ValueCommitTrapdoor::from_bytes(rng.rcv().to_repr()).unwrap();
+            let v_net = input.note.value() - NoteValue::from_raw(output.amount);
+            let cv_net = ValueCommitment::derive(v_net, rcv.clone());
+
+            let nf_old = input.note.nullifier(&fvk);
+            let ak: SpendValidatingKey = fvk.clone().into();
+            let alpha = rng.alpha();
+            let rk = ak.randomize(&alpha);
+
+            let note = Note::from_parts(
+                recipient,
+                NoteValue::from_raw(output.amount),
+                nf_old,
+                rng.rseed_new(&nf_old),
+            );
+
+            let cm_new = note.commitment();
+            let cmx = cm_new.into();
+
+            let encryptor = OrchardNoteEncryption::new(ovk, note, recipient, memo);
+            let epk = encryptor.epk();
+            let epk_slice = <OrchardDomain as Domain>::epk_bytes(epk);
+            let epk_bytes: [u8; 32] = epk_slice.as_ref().clone().try_into().unwrap();
+
+            let encrypted_note = TransmittedNoteCiphertext {
+                epk_bytes,
+                enc_ciphertext: encryptor.encrypt_note_plaintext(),
+                out_ciphertext: encryptor.encrypt_outgoing_plaintext(
+                    &cv_net,
+                    &cmx,
+                    &mut MockEncryptionRng::from(&rng),
+                ),
+            };
+
+            let auth = SigningMetadata {
+                dummy_ask,
+                parts: SigningParts {
+                    ak: ak.clone(),
+                    alpha,
+                },
+            };
+            let action = Action::from_parts(nf_old, rk, cmx, encrypted_note, cv_net, auth);
+            let spend = SpendInfo::new(fvk.clone(), input.note, input.merkle_path).unwrap();
+            let circuit = Circuit::from_action_context(spend, note, alpha, rcv.clone()).unwrap();
+
+            // update mutables
+            balance = (balance + v_net).expect("balance overflow");
+            rcv_sum = rcv_sum + &rcv;
+            circuits.push(circuit);
+            actions.push(action);
+        }
+        let balance: V = i64::try_from(balance)
+            .expect("overflow")
+            .try_into()
+            .map_err(|_| "cannot convert bundle balance")?;
+        Ok(Bundle::from_parts(
+            NonEmpty::from_vec(actions).unwrap(),
+            Flags::from_parts(true, true),
+            balance,
+            self.anchor,
+            InProgress {
+                proof: Unproven { circuits },
+                sigs: Unauthorized {
+                    bsk: rcv_sum.into_bsk(),
+                },
+            },
+        ))
+    }
+}
+
+fn pad<T>(xs: &mut Vec<Option<T>>, target_length: usize) {
+    for _ in 0..(target_length - xs.len()) {
+        xs.push(None);
+    }
+}
+
+fn parse_u_address(address: String) -> Result<Address, String> {
+    let (_, u_address): (Network, UnifiedAddress) =
+        Encoding::decode(&address[..]).map_err(|_| "cannot parse recipient address".to_owned())?;
+
+    let recipient = u_address
+        .items()
+        .into_iter()
+        .find_map(|x| match x {
+            Receiver::Orchard(addr) => Some(addr),
+            _ => None,
+        })
+        .ok_or("no orchard receiver in ua".to_owned())?;
+
+    let addr = Address::from_raw_address_bytes(&recipient).unwrap();
+    Ok(addr)
 }
---
+++ src/random.rs
@@ -0,0 +1,225 @@
+use blake2b_simd::Params;
+use orchard::{
+    keys::{SpendAuthorizingKey, SpendingKey},
+    note::Nullifier,
+    note::RandomSeed,
+    Address,
+};
+use pasta_curves::{
+    arithmetic::{CurveExt, FieldExt},
+    group::ff::{ PrimeField},
+    group::{ GroupEncoding},
+    pallas::{Point, Scalar},
+    Fp,
+};
+use rand_core::{CryptoRng, RngCore};
+pub struct BundleShieldingRng(pub [u8; 32]);
+
+fn sample_uniform(n: u32, rng: &mut impl RngCore) -> u32 {
+    loop {
+        let wide = rng.next_u32() as u64 * n as u64;
+        let high = (wide >> 32) as u32;
+        let low = (wide & u32::MAX as u64) as u32; // maybe just wide as u32 ?
+        if low <= u32::MAX - n || low <= u32::MAX - (u32::MAX - n) % n {
+            return high;
+        }
+    }
+}
+
+fn shuffle<T>(x: &mut [T], rng: &mut impl RngCore) {
+    for i in (1..x.len()).rev() {
+        let j = sample_uniform((i + 1) as u32, rng);
+        x.swap(i, j as usize);
+    }
+}
+
+impl BundleShieldingRng {
+    pub fn shuffle_inputs<T>(&self, inputs: &mut [T]) {
+        let mut rng = Blake2bCtrModeRng::new(&self.0, b"Inps_Permutation");
+        shuffle(inputs, &mut rng)
+    }
+    pub fn shuffle_outputs<T>(&self, outputs: &mut [T]) {
+        let mut rng = Blake2bCtrModeRng::new(&self.0, b"Outs_Permutation");
+        shuffle(outputs, &mut rng);
+    }
+    pub fn for_action(&self, i: u32) -> ActionShieldingRng {
+        let digest = Params::new()
+            .personal(b"ActionShieldSeed")
+            .hash_length(32)
+            .to_state()
+            .update(&self.0)
+            .update(&i.to_le_bytes())
+            .finalize();
+        ActionShieldingRng(digest.as_bytes().try_into().unwrap())
+    }
+}
+
+struct Blake2bCtrModeRng {
+    personal: [u8; 16],
+    seed: [u8; 32],
+    counter: u32,
+    buffer: [u8; 64],
+    offset: usize,
+}
+
+impl Blake2bCtrModeRng {
+    fn new(seed: &[u8; 32], personal: &[u8; 16]) -> Self {
+        Blake2bCtrModeRng {
+            personal: personal.clone(),
+            seed: seed.clone(),
+            counter: 0,
+            buffer: [0u8; 64],
+            offset: 64, // to trigger buffer initialization
+        }
+    }
+}
+
+impl RngCore for Blake2bCtrModeRng {
+    fn next_u32(&mut self) -> u32 {
+        if self.offset >= 64 {
+            let digest = Params::new()
+                .personal(&self.personal)
+                .hash_length(64)
+                .to_state()
+                .update(&self.seed)
+                .update(&self.counter.to_le_bytes())
+                .finalize();
+            self.buffer[..].copy_from_slice(digest.as_ref());
+            self.offset = 0;
+            self.counter += 1;
+        }
+        let array: [u8; 4] = self.buffer[self.offset..self.offset + 4]
+            .try_into()
+            .unwrap();
+        self.offset += 4;
+        ((array[0] as u32) << 0)
+            + ((array[1] as u32) << 8)
+            + ((array[2] as u32) << 16)
+            + ((array[3] as u32) << 24)
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        unimplemented!()
+    }
+    fn fill_bytes(&mut self, _dest: &mut [u8]) {
+        unimplemented!()
+    }
+    fn try_fill_bytes(&mut self, _dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        unimplemented!()
+    }
+}
+
+pub struct ActionShieldingRng(pub [u8; 32]);
+
+fn to_scalar(bytes: &[u8; 64]) -> Scalar {
+    Scalar::from_bytes_wide(bytes)
+}
+
+fn to_base(bytes: &[u8; 64]) -> Fp {
+    Fp::from_bytes_wide(bytes)
+}
+
+impl ActionShieldingRng {
+    fn random<const N: usize>(&self, dst: &[u8]) -> [u8; N] {
+        Params::new()
+            .personal(b"ActionExpandSeed")
+            .hash_length(N)
+            .to_state()
+            .update(&self.0)
+            .update(dst)
+            .finalize()
+            .as_bytes()
+            .try_into()
+            .unwrap()
+    }
+
+    pub fn alpha(&self) -> Scalar {
+        to_scalar(&self.random(b"alpha"))
+    }
+
+    pub fn rcv(&self) -> Scalar {
+        to_scalar(&self.random(b"rcv"))
+    }
+
+    pub fn dummy_recipient(&self) -> Address {
+        let mut raw_address = [0u8; 43];
+        let d: [u8; 11] = self.random(b"dummy_d");
+        let g_d = Point::hash_to_curve("z.cash:Orchard-gd")(&d);
+        let ivk = to_scalar(&self.random(b"dummy_ivk"));
+        let pk_d = g_d * ivk;
+        raw_address[..11].copy_from_slice(&d);
+        raw_address[11..].copy_from_slice(&pk_d.to_bytes());
+        Address::from_raw_address_bytes(&raw_address).unwrap()
+    }
+
+    pub fn dummy_ock(&self) -> [u8; 32] {
+        self.random(b"dummy_ock")
+    }
+
+    pub fn dummy_op(&self) -> [u8; 64] {
+        self.random(b"dummy_op")
+    }
+
+    pub fn dummy_rseed_old(&self, rho: &Nullifier) -> RandomSeed {
+        RandomSeed::from_bytes(self.random(b"dummy_rseed_old"), rho).unwrap()
+    }
+
+    pub fn rseed_new(&self, rho: &Nullifier) -> RandomSeed {
+        RandomSeed::from_bytes(self.random(b"rseed_new"), rho).unwrap()
+    }
+
+    pub fn dummy_sk(&self) -> SpendingKey {
+        SpendingKey::from_bytes(self.random(b"dummy_sk")).unwrap()
+    }
+
+    pub fn dummy_rho(&self) -> Nullifier {
+        Nullifier::from_bytes(&to_base(&self.random(b"dummy_rho")).to_repr()).unwrap()
+    }
+
+    pub fn dummy_ask(&self) -> SpendAuthorizingKey {
+        (&self.dummy_sk()).into()
+    }
+
+    /*
+    pub fn spend_auth_T(&self) -> [u8; 32] {
+        &self.random(b"spend_auth_T", 32)
+    */
+}
+
+pub struct MockEncryptionRng {
+    op: [u8; 64],
+    ock: [u8; 32],
+    state: usize,
+}
+
+impl From<&ActionShieldingRng> for MockEncryptionRng {
+    fn from(rng: &ActionShieldingRng) -> Self {
+        MockEncryptionRng {
+            op: rng.dummy_op(),
+            ock: rng.dummy_ock(),
+            state: 0,
+        }
+    }
+}
+
+impl RngCore for MockEncryptionRng {
+    fn next_u32(&mut self) -> u32 {
+        unimplemented!()
+    }
+    fn next_u64(&mut self) -> u64 {
+        unimplemented!()
+    }
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        match self.state {
+            0 => dest.copy_from_slice(&self.ock),
+            1 => dest.copy_from_slice(&self.op),
+            _ => panic!(),
+        }
+        self.state += 1;
+    }
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        unimplemented!()
+    }
+}
+
+impl CryptoRng for MockEncryptionRng {}
